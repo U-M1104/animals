8f1410dd296ae96e6c86bbfeebeb25eb2b2d00f4
U-M1104 <76837013+U-M1104@users.noreply.github.com>
2021-12-14 19:25:35 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function startclassification()
+{
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/7E1j6obyS/',modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
+
+function gotResults(error,results){
+if (error){
+    console.error(error);
+}
+else{
+    console.log(results);
+}
+}
